32cf4f6bbb5b1089ddaef92fc86c411ec814acff
kiwidaikon <niratamagomoti@yahoo.co.jp>
2025-01-19 16:35:01 +0900

    編集機能追加

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -20,7 +20,8 @@ class MemoController extends Controller
         return view('memos.create');
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         //インスタンスの作成
         $memo = new Memo();
         $memo->title = $request->title;
@@ -33,5 +34,26 @@ class MemoController extends Controller
     {
         $memo = Memo::find($id);
         return view('memos.show', ['memo' => $memo]);
+    }
+
+    public function edit($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.edit', ['memo' => $memo]);
+    }
+    public function update(Request $request, $id)
+    {
+        // ここはidで探して持ってくる以外はstoreと同じ
+        $memo = Memo::find($id);
+
+        // 値の用意
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+
+        // 保存
+        $memo->save();
+
+        // 登録したらindexに戻る
+        return redirect(route('memos.index'));
     }
 }
